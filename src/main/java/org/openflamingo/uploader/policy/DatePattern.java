37985ab2dc1d9568e99831396a18b4e5a52c61f6
Edward KIM <fharenheit@gmail.com>
2012-08-20 00:47:37 +0900

    라이센스 동의문 추가.

--- src/main/java/org/openflamingo/uploader/policy/DatePattern.java
+++ src/main/java/org/openflamingo/uploader/policy/DatePattern.java
@@ -1,19 +1,22 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Flamingo HDFS File Uploader - a tool to upload from datasource to datasource and schedule jobs
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright (C) 2011-2012 Cloudine.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This file is part of Flamingo HDFS File Uploader.
+ *
+ * Flamingo HDFS File Uploader is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Flamingo HDFS File Uploader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.openflamingo.uploader.policy;
 
@@ -52,10 +55,5 @@ public class DatePattern implements SelectorPattern {
         String evaluated = jobContext.getValue(filename);
         String dateFormat = jobContext.getValue(pattern);
         return org.springframework.util.StringUtils.countOccurrencesOf(evaluated, dateFormat) > 0;
-    }
-
-    @Override
-    public JobContext getJobContext() {
-        return jobContext;
     }
 }
