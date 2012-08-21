d5ac390aa6e3f2bc41c8a62a1c1f4bb82896c56e
Edward KIM <fharenheit@gmail.com>
2012-08-22 00:08:40 +0900

    HTTP 추가.

--- src/main/java/org/openflamingo/uploader/handler/HttpToLocalHandler.java
+++ src/main/java/org/openflamingo/uploader/handler/HttpToLocalHandler.java
@@ -38,6 +38,7 @@ import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import sun.misc.IOUtils;
 
+import java.nio.charset.Charset;
 import java.util.List;
 
 import static org.openflamingo.uploader.util.FileSystemUtils.*;
@@ -173,7 +174,7 @@ public class HttpToLocalHandler implements Handler {
         ClientHttpResponse response = null;
         try {
             response = request.execute();
-            responseBodyAsString = new String(IOUtils.readFully(response.getBody(), 4096, false));
+            responseBodyAsString = new String(IOUtils.readFully(response.getBody(), 4096, false), Charset.defaultCharset());
             jobLogger.debug("HTTP 요청의 응답 메시지는 다음과 같습니다.\n{}", responseBodyAsString);
             jobLogger.info("HTTP 요청을 완료하였습니다. 상태 코드는 '{}({})'입니다.", response.getStatusText(), response.getRawStatusCode());
             if (response.getRawStatusCode() != HttpStatus.ORDINAL_200_OK) {
